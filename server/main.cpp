d20321f7b9fea8d144611f784d3f950b4c6a3d23
Patrick Li <baichuanli@yahoo.com>
2025-11-22 17:44:38 -0500

    feat: initial server

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <grpcpp/grpcpp.h>
+#include "orphan_sock.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::Status;
+using grpc::ServerContext;
+
+using orphan_sock::OrphanSockServer;
+using orphan_sock::CloseRequest;
+using orphan_sock::Empty;
+
+class OrphanSockServerImpl final : public OrphanSockServer::Service {
+    public:
+        Status RegisterCloseProcess(ServerContext * ctx, const CloseRequest * req, Empty * reply) {
+            int64_t pid = req->process_id();
+
+            return Status::OK;
+        }
+
+};
 
 int main() {
-    std::cout << "Hello, world" << std::endl;
+    OrphanSockServerImpl service;
+    ServerBuilder builder;
+    std::string uds_path = "/tmp/orphan_sock.sock";
+    std::remove(uds_path.c_str());
+    builder.AddListeningPort("unix:" + uds_path, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server Starting..." << std::endl;
+    server->Wait();
 }
